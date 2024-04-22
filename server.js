671c3fe63cb6c3bed6db96761de2844f63e5dc3c
Joan <txuen006@gmail.com>
2024-04-22 17:54:37 +0800

    factor: pose functions

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(fileupload({ safeFileNames: true, preserveExtension: true }));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
-app.use(express.static("./backend"));
+app.use(express.static(__dirname+"/backend"));
 app.use("/", indexRouter);
 global.currentUser = null;
 
