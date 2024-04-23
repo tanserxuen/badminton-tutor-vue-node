31ebe039870651ff437583c48314dd835e1a9207
Joan <txuen006@gmail.com>
2024-04-23 16:34:10 +0800

    post: change layout of post creation

--- src/components/PostCreate.vue
+++ src/components/PostCreate.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="container px-4 py-10">
-    <h2 class="base-page__heading">Create Post</h2>
+    <h2 class="base-page__heading-short">Create Post</h2>
     <form
       class="mx-auto"
       style="max-width: 360px"
       encType="multipart/form-data"
       @submit.prevent="submitForm"
     >
+      <div>
+        <img
+          ref="avatar_preview"
+          class="object-cover rounded-full m-auto"
+          style="height: 250px; width: 250px"
+          :src="formData.image ? formData.image : '/images/placeholderImg.jpg'"
+          alt="Current profile photo"
+        />
+        <label class="block">
+          <span class="sr-only">Choose profile photo</span>
+          <input
+            ref="fileInput"
+            required
+            accept="image/png, image/gif, image/jpeg"
+            type="file"
+            name="image"
+            @change="uploadImage"
+            class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100 mb-5"
+          />
+        </label>
+      </div>
       <label class="auth-page__label" for="title">Title</label>
       <input
         type="text"
@@ -26,28 +47,7 @@
         @input="(event) => (formData.description = event.target.value)"
         required
       />
-      <div class="flex items-center space-x-6">
-        <div class="shrink-0">
-          <img
-            ref="avatar_preview"
-            class="h-32 w-32 object-cover rounded-full"
-            :src="formData.image ?? '/images/placeholderImg.jpg'"
-            alt="Current profile photo"
-          />
-        </div>
-        <label class="block">
-          <span class="sr-only">Choose profile photo</span>
-          <input
-            ref="fileInput"
-            required
-            accept="image/png, image/gif, image/jpeg"
-            type="file"
-            name="image"
-            @change="uploadImage"
-            class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
-          />
-        </label>
-      </div>
+
       <button class="auth-page__submit-button">Create</button>
     </form>
   </div>
