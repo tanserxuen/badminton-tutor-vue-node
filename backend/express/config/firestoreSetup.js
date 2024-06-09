28ae91912ac07e6b7417571fc7ab81d1b4a92934
Joan Xuen <txuen006@gmail.com>
2024-06-10 00:40:29 +0800

    fix: remove ./badmintonposecounter-8a231abc9956 setting this into env file

--- backend/express/config/firestoreSetup.js
+++ backend/express/config/firestoreSetup.js
@@ -1,9 +1,12 @@
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('./badmintonposecounter-8a231abc9956');
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+require("dotenv").config();
+const serviceAccount = JSON.parse(
+  process.env.BADMINTONPOSECOUNTER_SERVICE_ACCOUNT
+);
 
 initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
 
 global.db = getFirestore();
