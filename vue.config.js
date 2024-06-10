f8d69a4888f9ea285ec7994e661b029efa2d5c58
Joan Xuen <txuen006@gmail.com>
2024-06-10 22:41:45 +0800

    setup allowed host for 'Invalid Host header' error in deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: [
+      'localhost',
+      'badmintontutor-8d444ec927af.herokuapp.com',
+      'badmintontutor-backend-f45eaf6b326e.herokuapp.com',
+    ],
+  },
 })
