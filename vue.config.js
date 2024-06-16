910e79e42f36dca6e458638ab6113c1739a42cb0
Joan Xuen <txuen006@gmail.com>
2024-06-16 18:36:15 +0800

    testing for deployment in github pages

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: [
-      'localhost',
-      'badmintontutor-8d444ec927af.herokuapp.com',
-      'badmintontutor-backend-f45eaf6b326e.herokuapp.com',
+      "localhost",
+      "badmintontutor-8d444ec927af.herokuapp.com",
+      "badmintontutor-backend-f45eaf6b326e.herokuapp.com",
     ],
   },
-})
+  publicPath: process.env.NODE_ENV === "prod" ? "/REPO_NAME/" : "/",
+});
